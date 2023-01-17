af9945086724dd4e04628457c9bc48f72482ccd3
Artem Pralich <ArtemPralich>
2023-01-17 14:54:36 +0100

    added small views

---
+++ View/CompanyView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiMvvm.View;
+
+public partial class CompanyView : ContentPage
+{
+	public CompanyView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ View/EmployeeView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiMvvm.View;
+
+public partial class EmployeeView : ContentPage
+{
+	public EmployeeView()
+	{
+		InitializeComponent();
+	}
+}
